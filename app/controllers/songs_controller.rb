663de6eeed2c440d6d1fbf1f38276893a9242948
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-27 21:11:42 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,10 +1,10 @@
 class SongsController < ApplicationController
   def index
-    #@songs = Song.all
+    @songs = Song.all
     if params[:artist_name]
       raise params.inpsect
       @artist = Artist.find(params[:artist_name]).songs
-      redirect_to artist_path(@artist)
+      redirect_to artist_song_path(@artist, @song)
     else
       @songs = Song.all
     end
