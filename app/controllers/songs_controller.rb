34dc09081c7a9ed78872097612061d7e7c14aba8
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-27 21:01:54 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,6 +1,6 @@
 class SongsController < ApplicationController
   def index
-    @songs = Song.all
+    #@songs = Song.all
     if params[:artist_id]
       @songs = Artist.find(params[:artist_id]).songs
     else
